3a2d056041758b42e0d434ba382c64cbb13c7481
Sukma Rangga Pradita Wijaya <46914976+sukmaranggapradeta@users.noreply.github.com>
2019-02-09 06:33:19 +0700

    update metode sort

--- Exercises_15_Mengelompokkan_Hewan.js
+++ Exercises_15_Mengelompokkan_Hewan.js
@@ -1,35 +1,43 @@
-/* 
-Diberikan sebuah function groupAnimals(arr) yang menerima satu parameter berupa array yang berisi string. Function akan me-return array multidimensi dimana array tersebut berisikan kategori/kelompok array yang dikumpulkan sesuai dengan abjad depannya. Abjad harus berurut dari a-z (jika ada). Untuk kasus ini, anggap saja semua text lowercase.
-*/
- 
 function groupAnimals(animals) {
-  // you can only write your code here!
-  var deretanHuruf = "abcdefghijklmnopqrstuvwxyz"
-  var urutanBinatang = [];
-  var grubBinatang = [];
-  var indeks = 0;
- 
-  for (var x = 0 ; x < deretanHuruf.length ; x++){
-      for (var i = 0 ;i<animals.length ;i++){
-        var hurufDepan = animals[i].substr(0,1)
-        if (deretanHuruf[x] == hurufDepan){
-          urutanBinatang.push(animals[i])
-          //console.log(animals[i])    
-        }
+  var zoo = [];
+
+  for (var x = 0 ; x < animals.length ; x++){ 
+  var flag = false
+    //console.log(animals.length + " animals ==== zoo " + zoo.length)
+    var cage = []
+    for (var y = 0 ; y < zoo.length; y++){
+      //console.log(animals[x][0] + " === " + console.log(zoo[y][0][0])+"====")
+      //kalo di zoo ada binatang yg huruf depannya sama, maka dimasukin ke kandang yg sama
+      if (animals[x][0] === zoo[y][0][0]){
+        //console.log(animals[x] + " animals " + x)
+        zoo[y].push(animals[x])
+        flag = true
       }
     }
-  //return urutanBinatang 
-
- for (var i = 0; i < urutanBinatang.length; i++) {
-   if (grubBinatang.length === 0) {
-     grubBinatang.push([urutanBinatang[i]]);
-   } else if (grubBinatang[indeks][0][0] === urutanBinatang[i][0]) {
-     grubBinatang[indeks].push(urutanBinatang[i]);
-   } else {
-     grubBinatang.push([urutanBinatang[i]]);
-     indeks++;
-   }
-  } return grubBinatang;
+    //kalo di zoo blum ada animals yg huruf depannya sama, maka bikin kandang baru
+    if (flag === false){
+      //console.log(animals[x] + " +++++ animals " + x )
+      cage.push(animals[x])
+      zoo.push(cage)
+      flag = false;
+      //console.log(cage + " CAGE")
+      //console.log(zoo + " ZOO")
+    }
+  }
+  //return zoo
+  //mengurutkan nama binatang di zoo
+  for (var i = 0 ; i < zoo.length ; i++){
+  var kecil = []  
+    for (var j = 0 ; j < zoo.length; j++){    
+      if ((zoo[i][0][0] < zoo[j][0][0]) === true){
+      //console.log(zoo[i][0][0] + " < " + zoo[j][0][0] + " didalm if")
+        kecil = zoo[i]
+        zoo[i] = zoo[j]
+        zoo[j] = kecil
+      }
+    }  
+  }
+  return zoo
 }
 
 // TEST CASES
